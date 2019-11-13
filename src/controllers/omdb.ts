57975051cc5dbb8fbea1392c785e24a5c56452fb
James Dewar <james@arcs.in>
2019-11-12 22:32:41 -0500

    i should be awaiting

--- src/controllers/omdb.ts
+++ src/controllers/omdb.ts
@@ -9,36 +9,32 @@ class OMDBController {
   /**
    * Search the IMDB database through the OMDB interface
    */
-  search(query: {title: string, year?: number}, page: number = 1) {
+  async search(query: {title: string, year?: number}, page: number = 1) {
     let searchUri = `${this.apiUri}?apikey=${this.apiKey}&s=${query.title}&page=${page}`;
     if (query.year) {
       searchUri += `&y=${query.year}`;
     }
 
-    return rp.get({uri: searchUri, json: true})
-      .then(result => {
-        if (result.Response === 'False') {
-          return [];
-        } else {
-          return result.Search.map((film: any) => ({title: film.Title, year: film.Year, imdbID: film.imdbID}))
-        }
-      });
+    const result = await rp.get({uri: searchUri, json: true});
+    if (result.Response === 'False') {
+      return [];
+    } else {
+      return result.Search.map((film: any) => ({title: film.Title, year: film.Year, imdbID: film.imdbID}))
+    }
   }
 
   /**
    * Get an IMDB film by ID
    * @param imdbID IMDB ID of the film you're getting
    */
-  get(imdbID: string): Promise<any> {
+  async get(imdbID: string): Promise<any> {
     const getUri = `${this.apiUri}?apikey=${this.apiKey}&i=${imdbID}`;
 
-    return rp.get({ uri: getUri, json: true })
-      .then(result => {
-        if (result.Response === 'False') {
-          return null;
-        } else {
-          return {title: result.Title, year: result.year, length: result.length};
-        }
-      });
+    const result = await rp.get({ uri: getUri, json: true })
+    if (result.Response === 'False') {
+      return null;
+    } else {
+      return {title: result.Title, year: result.year, length: result.length};
+    }
   }
 }
